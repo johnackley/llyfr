0a045f7669651f84a7eca8c896636d18b243fc8e
John Ackley <john@johnackley.com>
2020-06-21 17:07:13 -0400

    jesting

--- server/src/goodreads/image-url.js
+++ server/src/goodreads/image-url.js
@@ -2,23 +2,28 @@ class ImageURL {
   constructor (data) {
     // console.log('imageURL data:', data);
     // TODO this is still buggy thanks to inconsistent from GR
-    if (data.url) {
+    // url exists
+    if (data.hasOwnProperty('url')) {
+      // try '_' first
       if (data.url['_']) {
-        this.url = data.url['_'];
+        this.url = data.url['_'].trim();
+        if (data.url['$']) {
+          this.nophoto = data.url['$'].nophoto === 'true';
+        }
       } else {
-        this.url = data.url;
+        this.url = data.url.trim();
       }
-    } else if (data.uri) {
+    } else if (data.hasOwnProperty('uri')) {
       if (data.uri['_']) {
-        this.url = data.uri['_'];
+        this.url = data.uri['_'].trim();
       } else {
-        this.url = data.uri;
+        this.url = data.uri.trim();
       }
+    } else if (data.hasOwnProperty('_')) {
+      this.url = data['_'].trim();
+      this.nophoto = data['$'].nophoto === 'true';
     } else {
-      this.url = data;
-    }
-    if (this.nophoto) {
-      this.nophoto = data.nophoto === 'true';
+      this.url = data.trim();
     }
     // console.log('parsed image-url:', this)
   }
